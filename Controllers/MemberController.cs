17b4d4ff73dd45a241102655521c17d4b959e143
HsiaoHungWang <wang@ispan.com.tw>
2025-04-19 10:43:11 +0800

    讀取及顯示資料庫中的圖

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OJTMApp.Models.ClassDB;
 
 namespace OJTMApp.Controllers
 {
     public class MemberController : Controller
     {
+        private readonly ClassDbContext db;
+        public MemberController(ClassDbContext classDbContext) {
+            db = classDbContext;
+        }
         public IActionResult Index()
         {
             return View();
@@ -18,5 +23,32 @@ namespace OJTMApp.Controllers
             //return Content(file);
             return File($"~/images/{file}", "image/jpeg");
         }
+
+        public async Task<IActionResult> Avatar(int id = 1)
+        {
+            Member? member = await db.Members.FindAsync(id);
+            if(member == null)
+            {
+                return NotFound();
+            }
+            //讀取會員的圖像檔名
+            //string? file = member.FileName;
+            //if (string.IsNullOrEmpty(file))
+            //{
+            //    return NotFound();
+            //}
+            //return File($"~/images/{file}", "image/jpeg"); 
+
+            //讀取會員的圖像二進位資料
+            byte[]? file = member.FileData;
+            if(file == null)
+            {
+                return NotFound();
+            }
+            return File(file, "image/jpeg");
+
+
+        }
+
     }
 }
