def320158f52c5d8844f8c5c5b32c3db6e55ae0b
HsiaoHungWang <wang@ispan.com.tw>
2025-04-22 20:16:59 +0800

    改用ViewModel接收資料

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OJTMApp.Models.ClassDB;
+using OJTMApp.Models.ViewModel;
 
 namespace OJTMApp.Controllers
 {
@@ -40,9 +41,13 @@ namespace OJTMApp.Controllers
         //怎麼接收表單送過來的資料 不管是GET還是POST
         //?userName=abc&userPassword=123
         [HttpPost]
-        public IActionResult Login(string userName, string userPassword)
+     //   public IActionResult Login(string userName, string userPassword)
+         public IActionResult Login(LoginViewModel loginVM)
         {
-           ViewBag.Message = $"使用者名稱:{userName}，密碼:{userPassword}";
+            string name = loginVM.userName ?? string.Empty;
+            string password = loginVM.userPassword ?? string.Empty  ;
+
+            ViewBag.Message = $"使用者名稱:{name}，密碼:{password}";
 
             return View();
 
---
+++ Models/ViewModel/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OJTMApp.Models.ViewModel
+{
+    public class LoginViewModel
+    {
+        //屬性名稱要跟表單的input name一樣
+        public string? userName { get; set; }
+        public string? userPassword { get; set; }
+    }
+}
