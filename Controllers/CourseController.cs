cf4c2e44eb729aa7d10ab6c411a93ee925a00921
HsiaoHungWang <wang@ispan.com.tw>
2025-04-26 11:33:51 +0800

    課程資料新增

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -1,17 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OJTMApp.Models.ClassDB;
 
 namespace OJTMApp.Controllers
 {
     public class CourseController : Controller
     {
+        private readonly ClassDbContext db;
+        public CourseController(ClassDbContext _dbContext) {
+            db = _dbContext;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
         //新增課程的View
+        [HttpGet]
         public IActionResult Create()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Course course)
+        {
+            //新增
+            await db.Courses.AddAsync(course);
+            await db.SaveChangesAsync();
             return View();
         }
     }
