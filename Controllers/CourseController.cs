f03cac644b0cac816a8cfae362f7f25ec057de8c
HsiaoHungWang <wang@ispan.com.tw>
2025-04-26 11:08:56 +0800

    asp-controller、asp-action 的使用

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OJTMApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //新增課程的View
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
