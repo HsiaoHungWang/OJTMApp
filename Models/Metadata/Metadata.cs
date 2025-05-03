4201af2bfd8da01b1e758e243374649e659a4e2b
HsiaoHungWang <wang@ispan.com.tw>
2025-05-03 13:52:19 +0800

    用 Partial Class 設定 Data Annotations

---
+++ Models/Metadata/Metadata.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OJTMApp.Models.Metadata
+{
+    public class ShipperMetadata
+    {
+        [Display(Name = "公司名稱")]
+        [Required(ErrorMessage = "公司名稱必填")]
+        public string CompanyName { get; set; } = null!;
+
+        [Display(Name = "電話")]
+        public string? Phone { get; set; }
+    }
+    //public class ProductMetadata
+    //{
+    //}
+    //public class MemberMetadata
+    //{
+    //}
+}
---
+++ Models/Metadata/PartialClass.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OJTMApp.Models.Metadata;
+
+namespace OJTMApp.Models
+{
+    [ModelMetadataTypeAttribute(typeof(ShipperMetadata))]
+    public partial class Shipper
+    {
+    }
+
+    //[ModelMetadataTypeAttribute(typeof(MemberMetadata))]
+    //public partial class Member
+    //{
+    //}
+}
