5c317bec87d6e3eac84d37cb5a63c19c827e7f16
HsiaoHungWang <wang@ispan.com.tw>
2025-05-03 10:35:18 +0800

    前端驗證範例

--- Models/ViewModel/RegisterViewModel.cs
+++ Models/ViewModel/RegisterViewModel.cs
@@ -8,11 +8,16 @@ namespace OJTMApp.Models.ViewModel
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "電子郵件必填")]
+        [EmailAddress(ErrorMessage ="電子郵件格式不正確")]
         public string? Email { get; set; }
 
+        [Range(18,100, ErrorMessage = "年紀要大於18小於100")]
         public int? Age { get; set; }
 
+        [RegularExpression(@"^\d{4,8}$", ErrorMessage = "密碼是4-8個數字")]
         public string? Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "密碼不一致")]
         public string? ConfirmPassword { get; set; }
     }
 }
