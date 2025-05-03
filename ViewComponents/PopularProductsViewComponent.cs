e9eda982c068c1a8bc0bc1dae85dd31cbcea4bb3
HsiaoHungWang <wang@ispan.com.tw>
2025-05-03 15:55:41 +0800

    熱門商品的ViewComponent

---
+++ ViewComponents/PopularProductsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OJTMApp.Models;
+
+namespace OJTMApp.ViewComponents
+{
+    public class PopularProductsViewComponent : ViewComponent
+    {
+        private readonly NorthwindContext _context;
+        public PopularProductsViewComponent(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int top = 3)
+        {
+
+            var products = await _context.Products.OrderByDescending(p => p.OrderDetails.Sum(od => od.Quantity))
+                .Take(top).ToListAsync();
+
+            return View("Default", products);
+        }
+    }
+}
